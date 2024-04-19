02cae0cee3fe1886abb7b160330ff857a05bc9ee
NathaliaCastro <62121458+NathaliaCastro@users.noreply.github.com>
2024-04-19 12:43:18 -0300

    Update ListaDeItens.cs
    Comentando as tentativas que falharam durante o desafio

--- PrimeiroDesafio/ListaDeItens.cs
+++ PrimeiroDesafio/ListaDeItens.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-// DESAFIO 2 - PRIMEIRO TESTE  >> 0 / 3 Testes de abertos tiveram sucesso. - FALHOU
+// PRIMEIRO TESTE DE EXECUÇÃO >> Retornado erro: 0 / 3 Testes tiveram sucesso.
 
 // List<string> itens = new List<string>(3);
 
@@ -21,22 +21,26 @@ using System.Threading.Tasks;
 
 
 
+// SEGUNDO TESTE DE EXECUÇÃO >> Retornado sucesso: 3 / 3 Testes tiveram sucesso. Clique em "ENTREGAR DESAFIO" para finalizar.
 
+List<string> itens = new List<string>();
+   //Lista para armazenar os itens
 
-// DESAFIO 2 - SEGUNDO TESTE  >> 3 / 3 Testes de abertos tiveram sucesso. Clique em "ENTREGAR DESAFIO" para finalizar.
-
-List<string> itens = new List<string>();     // Lista para armazenar os itens
-
-        // TODO: Solicite os itens ao usuário
 Console.WriteLine("Digite 3 itens para o seu personagem: \n");
-for (int contador = 0; contador <= 2; contador++)     //A lista inicia com índice 0 até 2, pois de zero a dois existem três espaços
+for (int contador = 0; contador <= 2; contador++)     
+   //A lista inicia com índice 0 até 2, pois de zero a dois existem três espaços
 {        
-      string item = Console.ReadLine();     //Declarando a variável ITEM como string e armazenando os itens inseridos
-      itens.Add(item);     //Adiciona os 3 itens desejados
+      string item = Console.ReadLine();
+    //Declarando a variável ITEM como string e armazenando os itens inseridos
+      itens.Add(item);
+    //Adiciona os 3 itens desejados
 }
 
-Console.WriteLine("\nLista de itens:");     // Exibe a lista de itens
-foreach (string item in itens)     //A variável ITEM do tipo string, guarda em ITENS os objetos adicionados anteriormente
+Console.WriteLine("\nLista de itens:");
+    // Exibe a lista de itens
+foreach (string item in itens)
+    //A variável ITEM do tipo string, guarda em ITENS os objetos adicionados anteriormente
 {
-      Console.WriteLine($"- {item}");      //Exibe em tela os itens adicionados em : itens.Add(item)
+      Console.WriteLine($"- {item}");
+    //Exibe em tela os itens adicionados em : itens.Add(item)
 }
